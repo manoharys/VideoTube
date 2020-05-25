f5bcbfc712b9764aa5ed8166341a34583480047b
manohar <manumanohar1527@gmail.com>
2020-05-25 14:39:08 +0530

    Clonning started😎

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouTube</title>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./assets/CSS/style.css">
+</head>
+<body>
+     YOUTUBE-ClONE
+</body>
+</html>
