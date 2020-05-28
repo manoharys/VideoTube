73e82107851d7f8e260012471c82c9940a735f6b
manohar <manumanohar1527@gmail.com>
2020-05-28 17:19:54 +0530

    connecting our database

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+ob_start(); //Turns on output buffering
+
+date_default_timezone_set("Asia/Kolkata");
+
+$servername  =  "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=VideoTube", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php require_once("includes/config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
