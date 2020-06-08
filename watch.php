773aac1012394dcbe913e92c36a93aee5af765d4
manohar <manumanohar1527@gmail.com>
2020-06-08 21:40:28 +0530

    taking video ID from the url

---
+++ watch.php
@@ -0,0 +1,11 @@
+<?php require_once('includes/header.php');?>
+
+ <?php 
+    if(!isset($_GET["id"])){
+        echo "no url passed on to the page";
+    }
+    
+ ?>
+
+<?php require_once('includes/footer.php') ?>
+ 
