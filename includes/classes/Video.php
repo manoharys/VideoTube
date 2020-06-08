9f2f53d2082ffabef3f7747b0cfe0d5e0f351be0
manohar <manumanohar1527@gmail.com>
2020-06-08 22:20:17 +0530

    finishing up helper function of video class

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -16,9 +16,55 @@ class User {
             $query->bindParam(":id", $input);
             $query->execute();
             $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
-        }
-                
+        }           
     }
 
+    public function getVideoId(){
+        return $this->sqlData["id"];
+    }
+    
+    
+    public function getVideoUploadedBy(){
+        return $this->sqlData["uploadedBy"];
+    }
+
+    
+    public function getVideoTitle(){
+        return $this->sqlData["title"];
+    }
+
+    
+    public function getVideoDescription(){
+        return $this->sqlData["description"];
+    }
+    
+    public function getVideoCategory(){
+        return $this->sqlData["category"];
+    }
+
+    
+    public function getVideoPrivacy(){
+        return $this->sqlData["privacy"];
+    }
+
+    
+    public function getVideoFilePath(){
+        return $this->sqlData["filePath"];
+    }
+
+    
+    public function getVideoUploadDate(){
+        return $this->sqlData["uploadDate"];
+    }
+
+    
+    public function getVideoView(){
+        return $this->sqlData["views"];
+    }
+
+    
+    public function getVideoDuration(){
+        return $this->sqlData["duration"];
+    }
 }
 ?>
