1b47ffb5faea051f3a7283d0c95cb1f3def54940
manohar <manumanohar1527@gmail.com>
2020-06-13 22:28:43 +0530

    Subscribing and Unsubscribing functionality

--- ajax/subscribe.php
+++ ajax/subscribe.php
@@ -3,7 +3,41 @@
    include("../includes/config.php");
 
    if(isset($_POST["userTo"]) && isset($_POST["userFrom"])){
-     echo "parameters passed sucessfully";
+       $userTo = $_POST["userTo"];
+       $userFrom = $_POST["userFrom"];
+
+       /* 1) check wheather user is already subscribed
+          2) If not subscribed insert into database
+          3) else delete from the database / unsubscribe
+          4) return number of subscriber 
+       */
+       $query = $conn->prepare("SELECT * FROM subscribers WHERE userTo = :userTo AND userFrom = :userFrom");
+       $query->bindParam(":userTo", $userTo);
+       $query->bindParam(":userFrom", $userFrom);
+       $query->execute();
+
+       if($query->rowCount() == 0){
+           //insert
+           $query = $conn->prepare("INSERT INTO subscribers(userTo, userFrom)
+                                  VALUES(:userTo, :userFrom)"); 
+           $query->bindParam(":userTo", $userTo);
+           $query->bindParam(":userFrom", $userFrom);
+           $query->execute();         
+        }
+       else {
+           //DELETE
+            $query = $conn->prepare("DELETE  FROM subscribers WHERE userTo = :userTo AND userFrom = :userFrom");
+            $query->bindParam(":userTo", $userTo);
+            $query->bindParam(":userFrom", $userFrom);
+            $query->execute();
+       }
+       //RETURN NO. OF SUBS
+       $query = $conn->prepare("SELECT * FROM subscribers WHERE userTo = :userTo ");
+       $query->bindParam(":userTo", $userTo);
+       $query->execute();
+
+       echo $query->rowCount();
+
    }
    else{
        echo "one or more parameter is not passed to subscribe.php";
--- assets/JS/userAction.js
+++ assets/JS/userAction.js
@@ -3,7 +3,7 @@ function subscribe(userTo, userFrom, button) {
     if (userTo == userFrom) {
         alert("you cannot subscribe it yourself");
     } else {
-        $.post("ajax/subscribe.php")
+        $.post("ajax/subscribe.php",{userTo:userTo, userFrom:userFrom})
             .done(function (data) {
                 console.log(data);
             })
