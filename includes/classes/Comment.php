b21a7a33d03e4ada532a3c6bc15e69dc745e0aba
manohar <manumanohar1527@gmail.com>
2020-06-18 06:24:33 +0530

    comment timestamp

--- includes/classes/Comment.php
+++ includes/classes/Comment.php
@@ -25,7 +25,7 @@
            $body = $this->sqlData["body"];
            $postedBy = $this->sqlData["postedBy"];
            $profileButton = ButtonProvider::createProfileButton($this->conn, $postedBy);
-           $timespan = ""; //TODO : get timespan
+           $timespan = $this->time_elapsed_string($this->sqlData["datePosted"]);
            $commentControlsObj = new CommentControls($this->conn, $this, $this->userLoggedInObj);
            $commentControls = $commentControlsObj->create();
            return "
@@ -50,6 +50,36 @@
               
               ";
        }
+
+       public function time_elapsed_string($datetime, $full = false) {
+        $now = new DateTime;
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+    
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+    
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+    
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
+
        public function getId(){
            return $this->sqlData["id"];
        }
