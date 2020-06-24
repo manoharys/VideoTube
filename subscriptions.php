01e003e9908c9fd203857c20e16e500a1f1ad5f9
manohar <manumanohar1527@gmail.com>
2020-06-24 06:29:44 +0530

    redirecting to signIn page if not loggedIn

--- subscriptions.php
+++ subscriptions.php
@@ -1,6 +1,10 @@
 <?php
 require_once("includes/header.php");
 
+if(!User::isLoggedIn()) {
+    header("Location: signIn.php");
+}
+
 $subscriptionsProvider = new SubscriptionsProvider($conn, $userLoggedInObj);
 $videos = $subscriptionsProvider->getVideos();
 
