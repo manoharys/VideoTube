9d1570f91d0b711650726f4bf9e972925d1f5f3d
manohar <manumanohar1527@gmail.com>
2020-06-24 06:23:38 +0530

    Trending videos page

--- trending.php
+++ trending.php
@@ -1,1 +1,19 @@
-<?php require_once('includes/header.php');?>
+<?php
+require_once("includes/header.php");
+require_once("includes/classes/TrendingProvider.php");
+
+$trendingProvider = new TrendingProvider($conn, $userLoggedInObj);
+$videos = $trendingProvider->getVideos();
+
+$videoGrid = new VideoGrid($conn, $userLoggedInObj);
+?>
+<div class="largeVideoGridContainer">
+    <?php
+    if(sizeof($videos) > 0) {
+        echo $videoGrid->createLarge($videos, "Trending videos uploaded in the last week", false);
+    }
+    else {
+        echo "No trending videos to show";
+    }
+    ?>
+</div>
