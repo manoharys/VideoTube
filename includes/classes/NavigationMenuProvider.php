bcad382842a78dbe8131e8444c1ccd741d19ebf8
manohar <manumanohar1527@gmail.com>
2020-06-24 06:07:11 +0530

    showing a list of subscribtion

--- includes/classes/NavigationMenuProvider.php
+++ includes/classes/NavigationMenuProvider.php
@@ -13,13 +13,13 @@ class NavigationMenuProvider {
         $menuHtml .= $this->createNavItem("Trending", "assets/images/icons/trending.png", "trending.php");
         $menuHtml .= $this->createNavItem("Subscriptions", "assets/images/icons/subscriptions.png", "subscriptions.php");
         $menuHtml .= $this->createNavItem("Liked Videos", "assets/images/icons/thumb-up.png", "likedVideos.php");
-
+       
         if(User::isLoggedIn()) {
             $menuHtml .= $this->createNavItem("Settings", "assets/images/icons/settings.png", "settings.php");
             $menuHtml .= $this->createNavItem("Log Out", "assets/images/icons/logout.png", "logout.php");
-        }
 
-        // Create subscriptions section
+            $menuHtml .= $this->createSubscriptionsSection();
+        }
 
         return "<div class='navigationItems'>
                     $menuHtml
@@ -33,6 +33,17 @@ class NavigationMenuProvider {
                         <span>$text</span>
                     </a>
                 </div>";
+    }
+
+    private function createSubscriptionsSection() {
+        $subscriptions = $this->userLoggedInObj->getSubscriptions();
+
+        $html = "<span class='heading'>Subscriptions</span>";
+        foreach($subscriptions as $sub) {
+            $subUsername = $sub->getUsername();
+            $html .= $this->createNavItem($subUsername, $sub->getUserProfilePic(), "profile.php?username=$subUsername");
+        }
+        return $html;
     }
 
 }
