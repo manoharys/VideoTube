78b4cd90fa305e0eca2932421e4e06a5f35a8df6
manohar <manumanohar1527@gmail.com>
2020-06-24 06:26:53 +0530

    subscriptions page

---
+++ subscriptions.php
@@ -0,0 +1,18 @@
+<?php
+require_once("includes/header.php");
+
+$subscriptionsProvider = new SubscriptionsProvider($conn, $userLoggedInObj);
+$videos = $subscriptionsProvider->getVideos();
+
+$videoGrid = new VideoGrid($conn, $userLoggedInObj);
+?>
+<div class="largeVideoGridContainer">
+    <?php
+    if(sizeof($videos) > 0) {
+        echo $videoGrid->createLarge($videos, "New from your subscriptions", false);
+    }
+    else {
+        echo "No videos to show";
+    }
+    ?>
+</div>
