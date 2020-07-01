00b6f711b736aeabe8d438578d8eb6e950b29a77
manohar <manumanohar1527@gmail.com>
2020-07-01 22:11:54 +0530

    edit video page

---
+++ editVideo.php
@@ -0,0 +1,21 @@
+<?php
+require_once("includes/header.php");
+require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoDetailsFormProvider.php");
+require_once("includes/classes/VideoUploadData.php");
+
+if(!User::isLoggedIn()) {
+    header("Location: signIn.php");
+}
+
+if(!isset($_GET["videoId"])) {
+    echo "No video selected";
+    exit();
+}
+
+$video = new Video($conn, $_GET["videoId"], $userLoggedInObj);
+if($video->getVideoUploadedBy() != $userLoggedInObj->getUsername()) {
+    echo "Not your video";
+    exit();
+}
+?>
